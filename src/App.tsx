3d3ef9950e7d025f06ede3dd4525da838643fd11
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-03-05 19:15:37 -0300

    added routes configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import GlobalStyle from './styles/global';
+import GlobalStyles from './styles/global';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
-    <GlobalStyle />
-    <h1>Hello World</h1>
-  </>
+  <Router>
+    <GlobalStyles />
+
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+  </Router>
 );
 
 export default App;
