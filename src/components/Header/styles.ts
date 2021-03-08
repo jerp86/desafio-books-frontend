0381154b2d1210c6d19e6aedfa4ae43d26fb168f
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-03-08 19:07:37 -0300

    updated Header style from px to rem

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,16 +1,37 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-  width: 1130px;
+  /* width: 1130px; */
+  width: 70.625rem;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   margin: 0 auto;
-  padding: 40px 0;
+  padding: 2.5rem 0;
 
   color: ${props => props.color || '#FFF'};
+
+  @media (max-width: 1180px) {
+    /* width: 840px; */
+    width: 52.5rem;
+    padding: 2rem 0;
+  }
+
+  @media (max-width: 900px) {
+    /* width: 560px; */
+    width: 35rem;
+    padding: 1.5rem 0;
+  }
+
+  @media (max-width: 600px) {
+    /* width: 280px; */
+    width: 17.5rem;
+    padding: 1rem 0;
+
+    flex-direction: column;
+  }
 
   svg {
     path {
@@ -25,10 +46,10 @@ export const ContainerLogo = styled.div`
 `;
 
 export const HeaderTitle = styled.span`
-  line-height: 40px;
-  font-size: 28px;
+  line-height: 2.5rem;
+  font-size: 1.75rem;
   font-weight: 300;
-  margin-left: 16px;
+  margin-left: 1rem;
 `;
 
 export const Welcome = styled.div`
@@ -38,14 +59,18 @@ export const Welcome = styled.div`
 
   span {
     font-weight: 400;
-    font-size: 12px;
-    line-height: 16px;
+    font-size: 0.75rem;
+    line-height: 1rem;
 
-    margin-right: 16px;
+    margin-right: 1rem;
 
     strong {
       font-weight: 500;
     }
+  }
+
+  @media (max-width: 600px) {
+    padding-top: 0.5rem;
   }
 
   svg {
