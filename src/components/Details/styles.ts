7f1d4e955b88255fc15e19dc08071e18d13cc80a
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-03-08 23:04:51 -0300

    updated Details style from px to rem

--- src/components/Details/styles.ts
+++ src/components/Details/styles.ts
@@ -6,7 +6,7 @@ import { shade } from 'polished';
 const appearFromLeft = keyframes`
   from {
     opacity: 0;
-    transform: translateX(-50px);
+    transform: translateX(-3.125rem);
   }
   to {
     opacity: 1;
@@ -26,17 +26,21 @@ export const Container = styled.div`
   background-color: rgba(0, 0, 0, 0.4);
   color: #333;
 
-  left: 0;
-  top: 0;
   height: 100%;
   width: 100%;
 
   animation: ${appearFromLeft} 1.5s;
 
-  @media (max-width: 800px) {
+  @media screen and (max-width: 900px) {
+    justify-content: flex-start;
+    padding-top: 2rem;
+  }
+
+  @media screen and (max-width: 600px) {
     flex-direction: column;
   }
-  overflow-y: auto;
+
+  /* overflow-y: auto; */
 `;
 
 export const CloseIcon = styled(IoMdClose)`
@@ -48,11 +52,11 @@ export const CloseIconContainer = styled.div`
   align-items: flex-end;
   justify-content: flex-end;
   width: 100%;
-  margin-right: 32px;
+  margin-right: 2rem;
 
-  @media (max-width: 800px) {
+  @media screen and (max-width: 600px) {
     position: absolute;
-    top: 10px;
+    top: 0.5rem;
   }
 `;
 
@@ -60,8 +64,8 @@ export const CloseIconField = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 32px;
-  height: 32px;
+  width: 2rem;
+  height: 2rem;
   border-radius: 50%;
   background: #ffffff;
   cursor: pointer;
@@ -76,34 +80,44 @@ export const CloseIconField = styled.div`
 `;
 
 export const ModalWindow = styled.div`
-  height: 608px;
-  margin-bottom: 60px;
-  width: 769px;
-  height: 608px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  @media (max-width: 800px) {
-    display: flex;
-    position: absolute;
+  width: 48.063rem;
+  height: 38rem;
 
-    height: 100%;
-    width: 288px;
-    top: 50px;
-    margin-bottom: 50px;
-    max-height: 860px;
+  animation: ${appearFromLeft} 1.5s;
+
+  @media screen and (max-width: 900px) {
+    width: 85%;
+  }
+
+  @media screen and (max-width: 650px) {
+    width: 70%;
+    padding-top: 5rem;
   }
 `;
 
 export const ModalContainer = styled.div`
   display: flex;
-  background-color: #fff;
-  box-shadow: 0px 16px 80px rgba(0, 0, 0, 0.32);
-  border-radius: 4px;
-  width: 100%;
-  height: 100%;
+  align-items: center;
+  justify-content: center;
 
-  @media (max-width: 800px) {
+  background-color: #fff;
+  box-shadow: 0px 1rem 5rem rgba(0, 0, 0, 0.32);
+  border-radius: 0.25rem;
+
+  /* @media screen and (max-width: 900px) {
+    align-items: center;
+    justify-content: center;
+  } */
+
+  @media screen and (max-width: 650px) {
     flex-direction: column;
     height: 100%;
+    /* align-items: center;
+    justify-content: center; */
     overflow-y: auto;
   }
 `;
@@ -112,20 +126,26 @@ export const ImageBookContainer = styled.img`
   width: 60%;
   height: 100%;
 
-  padding: 48px;
-  filter: drop-shadow(0px 12px 18px rgba(0, 0, 0, 0.3));
+  padding: 3rem;
+  filter: drop-shadow(0px 0.75rem 1.125rem rgba(0, 0, 0, 0.3));
 
-  @media (max-width: 800px) {
-    width: 100%;
-    max-height: 350px;
-    padding: 24px;
+  @media screen and (max-width: 800px) {
+    width: 50%;
+    height: 90%;
+    padding: 1.5rem;
+  }
+
+  @media screen and (max-width: 650px) {
+    /* height: 80%;
+    padding: 1rem; */
+    display: none;
   }
 `;
 
 export const Content = styled.div`
   width: 50%;
   height: 100%;
-  padding: 48px;
+  padding: 3rem;
   padding-left: 0;
 
   display: flex;
@@ -133,31 +153,31 @@ export const Content = styled.div`
   justify-content: center;
   align-items: stretch;
 
-  @media (max-width: 800px) {
+  @media screen and (max-width: 650px) {
     width: 100%;
-    max-height: 500px;
+    /* max-height: 31.25rem; */
 
-    padding: 25px;
+    padding: 0.5rem;
   }
 `;
 
 export const HeaderContent = styled.div`
   h2 {
     font-weight: 500;
-    font-size: 28px;
-    line-height: 40px;
+    font-size: 1.75rem;
+    line-height: 2.5rem;
   }
 
-  span:first-of-type {
+  span {
     font-weight: 400;
-    font-size: 12px;
-    line-height: 20px;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
     color: #ab2680;
   }
 `;
 
 export const InfoContainer = styled.div`
-  padding-top: 32px;
+  padding-top: 2rem;
 `;
 
 export const InfoRow = styled.div`
@@ -168,34 +188,34 @@ export const InfoRow = styled.div`
   h3,
   h4 {
     font-weight: 500;
-    font-size: 12px;
-    line-height: 20px;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
   }
 
   h3 {
-    line-height: 30px;
+    line-height: 1.875rem;
     text-transform: uppercase;
-    margin-bottom: 8px;
+    margin-bottom: 0.5rem;
   }
 
   span {
     font-weight: 400;
-    font-size: 12px;
-    line-height: 20px;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
     color: #999;
   }
 `;
 
 export const Review = styled.div`
-  margin-top: 32px;
+  margin-top: 2rem;
   overflow: auto;
-  margin-right: -45px;
+  /* margin-right: -45px; */
 
   &::-webkit-scrollbar {
     width: 4px;
-    height: 48px;
+    height: 3rem;
 
-    border-radius: 30px;
+    border-radius: 1.875rem;
   }
 
   /* Track */
@@ -206,7 +226,7 @@ export const Review = styled.div`
   /* Handle */
   &::-webkit-scrollbar-thumb {
     background: #ab2680;
-    border-radius: 30px;
+    border-radius: 1.875rem;
   }
 
   /* Handle on hover */
@@ -216,15 +236,15 @@ export const Review = styled.div`
 
   h3 {
     font-weight: 500;
-    font-size: 12px;
-    line-height: 30px;
+    font-size: 0.75rem;
+    line-height: 1.875rem;
 
     text-transform: uppercase;
-    margin-bottom: 8px;
+    margin-bottom: 0.5rem;
   }
 
-  @media (max-width: 800px) {
-    margin-right: 0px;
+  @media screen and (max-width: 650px) {
+    margin-right: 0;
   }
 `;
 
@@ -233,18 +253,18 @@ export const DescriptionContainer = styled.div`
   color: #999;
 
   span {
-    width: 276px;
-    margin-left: 4px;
+    width: 17.25rem;
+    margin-left: 0.25rem;
     font-style: normal;
     font-weight: 500;
-    font-size: 12px;
-    line-height: 20px;
-    padding-right: 13px;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
+    padding-right: 0.813rem;
   }
 `;
 
 export const Quotes = styled(ImQuotesLeft)`
-  margin-right: 5px;
-  font-size: 15px;
+  margin-right: 0.313rem;
+  font-size: 0.938rem;
   color: ${shade(0.4, '#999')};
 `;
